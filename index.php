16cd7293942553bf88dbdb77de1cf3e26022a54d
czDamian <olebueziedamian@gmail.com>
2026-08-24 15:53:23 +0100

    testing br

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php
-echo ("Hello Vercy!");
-
-echo ("I'm back to php!");
+echo ("Hello Vercy! <br>");
+echo ("I'm back to php! <br>");
 
 $amount = 5000;
 $rate = 500;
 $myName = "Somto";
 
 echo ($amount * $rate);
-echo ($myName. "is owning me". $amount. "at rate of". $rate. "per day");
+echo ($myName . "is owning me" . $amount . "at rate of" . $rate . "per day <br>");
+
+//calculate the price to buy goat meat
+function goatMeat($quantity, $price)
+{
+  echo ($quantity * $price);
+}
+
+goatMeat(2, 500);
 
 ?>
