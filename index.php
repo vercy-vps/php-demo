7667a7b4025950157f352094269a7bebbe95939c
czDamian <olebueziedamian@gmail.com>
2026-08-24 14:48:10 +0100

    demo commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo ("Hello Vercy!");
+
+echo ("I'm back to php!");
+
+$amount = 5000;
+$rate = 500;
+$myName = "Somto";
+
+echo ($amount * $rate);
+echo ($myName. "is owning me". $amount. "at rate of". $rate. "per day");
+
+?>
