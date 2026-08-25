15242bf6ce4802623597558088cf4ba025e2f226
czDamian <olebueziedamian@gmail.com>
2026-08-25 14:37:44 +0100

    hello james

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
+//add html styling
+
 echo ("Hello there! <br>");
 
 $amount = 300;
 $rate = 50;
 $myName = "James";
 
-echo ($amount * $rate. "<br>");
+echo ($amount * $rate . "<br>");
 echo ($myName . " is owning me " . $amount . " at rate of " . $rate . " per day <br>");
 
 //calculate the price to buy goat meat
 function goatMeat($quantity, $price)
 {
-  echo ($quantity * $price);
+  echo (" the price of goat meat is " . $quantity * $price);
 }
 
-goatMeat(2, 500);
+goatMeat(15, 50);
 
 ?>
