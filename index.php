a7e1670a86c9df2709d76882695851e2b0ca107f
czDamian <olebueziedamian@gmail.com>
2026-09-03 04:33:13 +0100

    latest version

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //add html styling
 
 echo ("Hello there! <br>");
-echo ("Its gonna be my time. To show them that I can......! <br>");
+echo ("This is the latest version......! <br>");
 
 $amount = 300;
 $rate = 50;
